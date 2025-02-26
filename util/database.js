4ad9e91d6d23b991588dafba50abe3240adc7fba
Mohsin1016 <muhammadmohsin1016@gmail.com>
2025-02-26 07:05:31 +0500

    changes

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 // Replace the following with your AWS RDS details
-const sequelize = new Sequelize('fashion', 'admin', '1a2b3c4d5e6f7g8h9i', {
-  host: 'fashion-mysql-1.cl0wgciw6op8.ap-south-1.rds.amazonaws.com',
+const sequelize = new Sequelize('mern-mysql', 'admin', '1a2b3c4d5e6f7g8h9i', {
+  host: 'mern-mysql.cnmym8uquncp.eu-north-1.rds.amazonaws.com',
   dialect: 'mysql',
   port: 3306, // Explicitly specify the port (optional if default is 3306)
   logging: false, // Disable logging for cleaner output (optional)
